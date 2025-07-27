500acbb439653bf7a0b57f0b54b12d6d0cc8a7dd
arjunanoop101 <arjunanoop101@gmail.com>
2025-07-27 16:00:11 +0530

    ui changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,25 +7,19 @@ const Navbar = () => {
   const { navigate, token } = useAppContext();
 
   return (
-    <div className="flex justify-between items-center py-5 sm:mx-20 xl:mx-32 ">
-      {/* <img
-        onClick={() => navigate("/")}
-        src={assets.logo}
-        alt="logo"
-        className="w-32 sm:w-44"
-      /> */}
+    <div className="flex justify-between items-center py-4 px-4 sm:px-0 sm:mx-20 xl:mx-32">
       <h2
-        className="text-3xl font-bold text-primary cursor-pointer"
+        className="text-xl sm:text-3xl font-bold text-primary cursor-pointer"
         onClick={() => navigate("/")}
       >
         Arjun Anoop
       </h2>
       <button
         onClick={() => navigate("/admin")}
-        className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        className="flex items-center gap-2 rounded-full text-xs sm:text-sm cursor-pointer bg-primary text-white px-6 sm:px-10 py-2 sm:py-2.5"
       >
         {token ? "Dashboard" : "Login"}
-        <img src={assets.arrow} alt="arrow" className="w-3" />
+        <img src={assets.arrow} alt="arrow" className="w-2 sm:w-3" />
       </button>
     </div>
   );
